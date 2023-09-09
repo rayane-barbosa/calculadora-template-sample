82e72cc0767c7e4fc423f8c4ddc8dff7296519cd
Rayane Barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-09-09 18:41:30 +0000

    featur: showing numbers in display

--- js/app.js
+++ js/app.js
@@ -1,31 +1,51 @@
 var Calculator = /** @class */ (function () {
     function Calculator() {
-    }
-    var _a;
-    _a = Calculator;
-    Calculator.buttons = document.querySelectorAll(".tecla");
-    Calculator.display = document.getElementById("display");
-    Calculator.displayValue = "0";
-    Calculator.updateDisplay = function () {
-        _a.display.textContent = _a.displayValue;
-    };
-    Calculator.inputDisplay = function (digit) {
-        if (_a.displayValue.length >= 8) {
-            return; // Limite máximo de 8 dígitos
-        }
-        if (_a.displayValue === "0")
-            _a.displayValue = "";
-        _a.displayValue += digit;
-        _a.updateDisplay();
-    };
-    Calculator.start = function () {
-        _a.buttons.forEach(function (button) {
-            button.addEventListener("click", function () {
-                var buttonText = button.getAttribute("id");
-                _a.inputDisplay(buttonText);
-            });
+        var _this = this;
+        this.display = document.getElementById("display");
+        this.displayValue = "0";
+        this.previousValue = null;
+        this.operator = null;
+        this.waitingForSecondValue = false;
+        this.decimal = false;
+        this.updateDisplay();
+        var buttons = document.querySelectorAll(".tecla");
+        buttons.forEach(function (button) {
+            button.addEventListener("click", function () { return _this.handleButtonClick(button); });
         });
+    }
+    // atualizar o display
+    Calculator.prototype.updateDisplay = function () {
+        this.display.textContent = this.displayValue;
+    };
+    Calculator.prototype.inputDigit = function (digit) {
+        if (this.displayValue.length >= 8)
+            return;
+        if (this.waitingForSecondValue) {
+            this.displayValue = digit;
+            this.waitingForSecondValue = false;
+        }
+        else {
+            this.displayValue =
+                this.displayValue === "0" ? digit : this.displayValue + digit;
+        }
+        this.updateDisplay();
+    };
+    Calculator.prototype.handleButtonClick = function (button) {
+        var buttonText = button.getAttribute("id");
+        switch (buttonText) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                this.inputDigit(buttonText);
+        }
     };
     return Calculator;
 }());
-Calculator.start();
+new Calculator();
--- js/app.ts
+++ js/app.ts
@@ -1,33 +1,63 @@
 class Calculator {
-  static buttons = document.querySelectorAll(".tecla");
-  static display = document.getElementById("display") as HTMLSpanElement;
+  // mostrar os valores no display
+  private display: HTMLSpanElement;
+  private displayValue: string;
+  private previousValue: string | null;
+  private operator: string | null;
+  private waitingForSecondValue: boolean;
+  private decimal: boolean;
 
-  static displayValue: string = "0";
+  constructor() {
+    this.display = document.getElementById("display") as HTMLSpanElement;
+    this.displayValue = "0";
+    this.previousValue = null;
+    this.operator = null;
+    this.waitingForSecondValue = false;
+    this.decimal = false;
+    this.updateDisplay();
 
-  static updateDisplay = () => {
+    const buttons = document.querySelectorAll(".tecla");
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => this.handleButtonClick(button));
+    });
+  }
+
+  // atualizar o display
+  private updateDisplay() {
     this.display.textContent = this.displayValue;
-  };
+  }
 
-  static inputDisplay = (digit: string) => {
-    if (this.displayValue.length >= 8) {
-      return; 
+  private inputDigit(digit: string) {
+    if (this.displayValue.length >=8) return;
+    if (this.waitingForSecondValue) {
+      this.displayValue = digit;
+      this.waitingForSecondValue = false;
+    } else {
+      this.displayValue =
+        this.displayValue === "0" ? digit : this.displayValue + digit;
     }
 
-    if (this.displayValue === "0") this.displayValue = "";
-    this.displayValue += digit;
-
     this.updateDisplay();
-  };
+  }
 
-  static start = () => {
-    this.buttons.forEach((button) => {
-      button.addEventListener("click", () => {
-        const buttonText = button.getAttribute("id");
+  private handleButtonClick(button: Element) {
+    const buttonText = button.getAttribute("id");
 
-        this.inputDisplay(buttonText!);
-      });
-    });
-  };
+    switch (buttonText) {
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        this.inputDigit(buttonText);
+    }
+  }
 }
 
-Calculator.start();
+
+new Calculator();
